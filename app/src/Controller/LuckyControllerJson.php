a64f7eb13aaf51f6969ac363f1552bf251cb0da7
Grimgoren <adams_1992@hotmail.se>
2024-04-04 19:29:53 +0200

    Add remaining project files

---
+++ app/src/Controller/LuckyControllerJson.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerJson
+{
+    #[Route("/api/lucky/number")]
+    public function jsonNumber(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'lucky-number' => $number,
+            'lucky-message' => 'Hi there!',
+        ];
+
+        // return new JsonResponse($data);
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+    #[Route("/api")]
+    public function apiRoutes(): Response
+    {
+        $data = [
+            'routes' => [
+                'me' => '/',
+                'about' => '/about',
+                'report' => '/report',
+                'lucky' => '/lucky',
+                'lucky_pretty_print' => '/api/lucky/number',
+                'quote' => '/api/quote'
+            ]
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+    #[Route("/api/quote")]
+    public function apiQuote(): Response
+    {
+        $number = random_int(0, 2);
+
+        $t=time();
+        $time = date("Y-m-d H:i:s", $t);
+
+        $data = [];
+
+        if ($number === 0) {
+            $data = [
+                'Quote 1' => 'Good-bye. I am leaving because I am bored. - George Saunders, last words.' . ' ' . 'Generated: ' . $time,
+            ];
+        } elseif ($number === 1) {
+            $data = [
+                'Quote 2' => "Defer not till tomorrow to be wise, tomorrow's sun to thee may never rise. - William Congreve (1670 - 1729)." . " " . "Generated: " . $time,
+            ];
+        } elseif ($number === 2) {
+            $data = [
+                'Quote 3' => 'Those who cannot remember the past are condemned to repeat it. - George Santayana (1863 - 1952), The Life of Reason, Volume 1, 1905.' . ' ' . 'Generated: ' . $time,
+            ];
+        }
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+        $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
